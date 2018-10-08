ca217b8f17104721af0e2124db8a904e4c522996
Nataliya Buyakova <natasha@softdeveloper.ru>
2018-10-08 19:28:32 +0300

    перламутровые пуговицы

--- js/payment.js
+++ js/payment.js
@@ -11,6 +11,7 @@
   var cardDate = paymentCard.querySelector('#payment__card-date');
   var cardCvc = paymentCard.querySelector('#payment__card-cvc');
   var cardHolder = paymentCard.querySelector('#payment__cardholder');
+  // var btnSend = document.querySelector('.buy__submit-btn');
 
   // проверяем валидность введенного номера карты с помощью алгоритма луна
   var checkCardNumber = function (number) {
@@ -32,8 +33,9 @@
   // меняем статус в зависимости от введенных данных
   var changeStatus = function () {
     var isNumberValid = checkCardNumber(cardNumber.value);
-    var isCardValid = cardNumber.validity.valid && cardDate.validity.valid && cardCvc.validity.valid && cardHolder.validity.valid && isNumberValid;
-    var statusText = isCardValid === true ? 'Одобрен' : 'Не определен';
+    var isValidCvc = parseInt(cardCvc.value, 10) > 99 && parseInt(cardCvc.value, 10) < 1000;
+    var isCardValid = cardNumber.validity.valid && cardDate.validity.valid && cardCvc.validity.valid && cardHolder.validity.valid && isNumberValid && isValidCvc;
+    var statusText = isCardValid ? 'Одобрен' : 'Не определен';
     cardStatus.textContent = statusText;
   };
 
@@ -62,7 +64,7 @@
   cardCvc.addEventListener('invalid', function () {
     var validityText = '';
     if (cardCvc.validity.tooShort || cardCvc.validity.tooLong || cardCvc.validity.patternMismatch) {
-      validityText = 'Поле должно содержать 3 цифры';
+      validityText = 'Поле должно содержать 3 цифры, диапазон допустимых значений от 100 до 999';
     } else if (cardCvc.validity.valueMissing) {
       validityText = 'Поле обязательно к заполнению';
     }
@@ -91,8 +93,10 @@
     inputs.forEach(function (input) {
       if (element.classList.contains('visually-hidden')) {
         input.disabled = true;
+        input.required = '';
       } else {
         input.disabled = false;
+        input.required = 'required';
       }
     });
   };
@@ -113,7 +117,9 @@
 
 
   // вешаем обработчик на весь блок и меняем статус при изменении
-  paymentCard.addEventListener('change', changeStatus);
+  paymentCard.addEventListener('change', function () {
+    changeStatus();
+  });
 
   // вешаем обработичк на кнопки и переключаем вкладки при клике
   paymentBtn.addEventListener('click', paymentBtnHandler);
